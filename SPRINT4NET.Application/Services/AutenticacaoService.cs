f94a6eb04372236ea5bf0eb5e2a2d4f3fd5d60d3
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-11-01 16:49:55 -0300

    Camada Service da Autenticação criada.

---
+++ SPRINT4NET.Application/Services/AutenticacaoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SPRINT4NET.SPRINT4NET.Application.Services
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> AuthenticateUser(UserCredentials credentials)
+        {
+            var jsonContent = JsonSerializer.Serialize(credentials);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("URL_DA_API_DE_AUTENTICACAO", content);
+
+            return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : null;
+        }
+    }
+}
